154c850db62d3e3235b4ef4a197f5d1e88ebae0d
bdsmgeek <bdsmgeek@gmail.com>
2017-02-07 16:34:37 -0500

    Added styles and scripts

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  var animation = new Image();
+  
+  $("#gamegif").click(function() {
+    $("#gamegif").attr('src', 'http://static.tumblr.com/dqaghwj/j3pokywc0/spank-start.gif');
+  });
+});	
