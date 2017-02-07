007d8d0e6f9d54f5f291fe6c96e024bf10f4bf35
bdsmgeek <bdsmgeek@gmail.com>
2017-02-07 18:10:35 -0500

    fixed preloading and efficiency

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   var animation = new Image();
   $(animation).attr('src', 'img/spank-animated.gif');
   $("#gamegif").click(function() {
-    $("#gamegif").attr('src', animation);
+    $("#gamegif").attr('src', 'img/spank-animated.gif');
   });
 });	
