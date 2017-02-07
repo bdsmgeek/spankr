5f428631767674704e3f409246706610d52742cf
bdsmgeek <bdsmgeek@gmail.com>
2017-02-07 17:16:39 -0500

    updated pathing and animation reset.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var animation = new Image();
-  
+  animation.attr('src', '/img/spank-animated.gif');
   $("#gamegif").click(function() {
-    $("#gamegif").attr('src', '/img/spank-start.gif');
+    $("#gamegif").attr('src', animation);
   });
 });	
