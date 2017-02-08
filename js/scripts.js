beda23fefa330139aa374d7d89730efd33f517e5
bdsmgeek <bdsmgeek@gmail.com>
2017-02-07 20:31:36 -0500

    added counter and formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
+
   var animation = new Image();
   $(animation).attr('src', 'img/spank-animated.gif');
+
   $("#gamegif").click(function() {
     $("#gamegif").attr('src', 'img/spank-animated.gif');
+    $('#spankcounter').html(function(i, val) { return val*1+1 });
   });
+  
 });	
