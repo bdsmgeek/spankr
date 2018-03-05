7747b4bfc0a22d662c026a74b84a34995c72df53
BDSMGeek <bdsmgeek@gmail.com>
2018-03-04 20:19:36 -0500

    Added character selection with 1 new character and sound effect with option to mute. Minor adjustements to code for better visual effect.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,36 @@
 $(document).ready(function() {
 
-  var animation = new Image();
-  $(animation).attr('src', 'img/spank-animated.gif');
+  var animations = ['img/01.gif','img/02.gif'];
+  var thumbs = ['img/01.thumb.gif','img/02.thumb.gif'];
+  var spanks = 0;
+  var currentCharacter = 1;
+  var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', 'audio/spank.mp3');
 
-  $("#gamegif").click(function() {
-    $("#gamegif").attr('src', 'img/spank-animated.gif');
-    $('#spankcounter').html(function(i, val) { return val*1+1 });
+  $("#next").on( "click", function() {
+    next();
+  });
+  $("#mute").on( "click", function() {
+    mute();
+  });
+
+  function next() {
+    currentCharacter++;
+    if (currentCharacter == animations.length){
+      currentCharacter = 0;
+    };
+    $('#gamegif').attr('src', thumbs[currentCharacter]);
+  }
+
+  function mute(){
+    audioElement.muted = !audioElement.muted;
+    
+  }
+
+  $('#gamegif').click(function() {
+    $('#gamegif').attr('src', animations[currentCharacter]);
+    audioElement.play();
+    spanks++;
+    $('#spankcounter').text(spanks);
   });
 });
